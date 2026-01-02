8b7e47c6cd2d6f3ca147b95946c655577290e272
Victor Coelho <victor.coelho@fgv.br>
2026-01-02 08:15:38 -0300

    finsh course - everything is working

--- djangobnb_backend/property/api.py
+++ djangobnb_backend/property/api.py
@@ -29,15 +29,51 @@ def properties_list(request):
     
     #
     # Filter
-    is_favorites = request.GET.get('is_favorites', '')
-    landlord_id = request.GET.get('landlord_id', '')
+    country = request.GET.get('country', '')
+    category = request.GET.get('category', '')
+    checkin_date = request.GET.get('checkin', '')
+    checkout_date = request.GET.get('checkout', '')
+    guests = request.GET.get('numGuests', '')
+    bathrooms = request.GET.get('numBathrooms', '')
+    bedrooms = request.GET.get('numBedrooms', '')
     
+    is_favorites = request.GET.get('is_favorites', '')
+    
+    landlord_id = request.GET.get('landlord_id', '')
+        
+    if checkin_date and checkout_date:
+        exact_matches = Reservation.objects.filter(start_date=checkin_date) | Reservation.objects.filter(end_date=checkout_date)
+        overlap_matches = Reservation.objects.filter(start_date__lte=checkin_date, end_date__gte=checkout_date)
+        all_matches = []
+        
+        for reservation in exact_matches | overlap_matches:
+            all_matches.append(reservation.property.id)
+            serializer = PropertiesListSerializer(reservation.property, many=False)
+
+        properties = properties.exclude(id__in=all_matches)
+    
+    if properties:
+        serializer = PropertiesListSerializer(properties, many=True)
+    if guests:
+        properties = properties.filter(guests__gte=guests)
+
+    if bedrooms:
+        properties = properties.filter(bedrooms__gte=bedrooms)
+    
+    if bathrooms:
+        properties = properties.filter(bathrooms__gte=bathrooms)
+        
+    if country:
+        properties = properties.filter(country=country)
+    
+    if category and category != 'undefined':
+        properties = properties.filter(category=category)
+
     if is_favorites:
         properties = properties.filter(favorited__in=[request.user])
     
     if landlord_id:
         properties = properties.filter(landlord_id=landlord_id)
-    
     
     #
     # Favoroties
@@ -46,12 +82,8 @@ def properties_list(request):
             if request.user in property.favorited.all():
                 favorites.append(property.id)
     
-    print(f'{favorites=}')
-    #
-    #
     
     serializer = PropertiesListSerializer(properties, many=True)
-    
     return JsonResponse({
         'data': serializer.data,
         'favorites': favorites,
