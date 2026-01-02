58e226c71bacc119f31a2d0193a1861c9efeba53
Victor <coelhovictor0715@gmail.com>
2026-01-02 19:02:15 -0300

    set DEBUG to False

--- djangobnb_backend/djangobnb_backend/settings.py
+++ djangobnb_backend/djangobnb_backend/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = bool(os.environ.get('DEBUG', default=0))
+DEBUG = False
 
 if DEBUG:
     ALLOWED_HOSTS = ["localhost", "127.0.0.1", "147.182.200.106"]
