32b5f1e776c5a96e1e3cffd2b1d9a77a7996906b
Victor <coelhovictor0715@gmail.com>
2026-01-02 10:52:15 -0300

    add dotenv

--- djangobnb_backend/djangobnb_backend/settings.py
+++ djangobnb_backend/djangobnb_backend/settings.py
@@ -4,6 +4,10 @@ from datetime import timedelta
 
 from pathlib import Path
 
+from dotenv import load_dotenv
+
+load_dotenv()
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
