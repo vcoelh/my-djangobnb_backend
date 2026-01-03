b70a1ec89a1ed6fc8e3362df455ed068ca6b33ec
Victor <coelhovictor0715@gmail.com>
2026-01-03 10:44:35 -0300

    change setting.py to use env variables with os.getenv

--- djangobnb_backend/djangobnb_backend/settings.py
+++ djangobnb_backend/djangobnb_backend/settings.py
@@ -17,10 +17,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY')
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = os.getenv('DEBUG', default=0)
 
 if DEBUG:
     ALLOWED_HOSTS = ["localhost", "127.0.0.1", "147.182.200.106"]
